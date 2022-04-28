a706cbb041cb404acc231bcae5ece1f9da7032d8
kirthigavadivelu <104414516+kirthigavadivelu@users.noreply.github.com>
2022-04-28 09:50:58 +0530

    Update HelloWorld.java

--- corejava/src/main/java/corejava/HelloWorld.java
+++ corejava/src/main/java/corejava/HelloWorld.java
@@ -3,6 +3,8 @@ package corejava;
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hi am kirthiga");
+		String str="kirthiga";
+		System.out.println(str);
 	
 	}
 
