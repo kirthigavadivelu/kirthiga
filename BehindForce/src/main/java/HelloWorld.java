7fcd62bc24fc9241b53abd6125e9e037eb5e5028
kirthiga.vadivelu <kirthiga.vadivelu@DT150152.QICHOAPPL.com>
2022-04-28 17:39:13 +0530

    28April

--- BehindForce/src/main/java/HelloWorld.java
+++ BehindForce/src/main/java/HelloWorld.java
@@ -1,9 +1,20 @@
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class HelloWorld {
+	static Logger logger = Logger.getLogger(HelloWorld.class);
 
 	public static void main(String[] args) {
-		
 
+		String str = "welcome ";
+
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:" + str);
+		logger.warn("warning message...:" + str);
+		logger.error("error message...:" + str);
 	}
 
 }
