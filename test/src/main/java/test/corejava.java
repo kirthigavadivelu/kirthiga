563a880d37a835ba74cc86f50d9564bebcb0147b
kirthiga.vadivelu <kirthiga.vadivelu@DT150152.QICHOAPPL.com>
2022-04-28 10:39:00 +0530

    28-april

--- test/src/main/java/test/corejava.java
+++ test/src/main/java/test/corejava.java
@@ -1,10 +1,10 @@
+package test;
 
-class HelloWorld {
+public class corejava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
-		
+
 	}
 
 }
