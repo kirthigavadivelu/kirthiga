0532f36e1fc92fbe1775c559d5f94d96ebb7eaf0
kirthiga.vadivelu <kirthiga.vadivelu@DT150152.QICHOAPPL.com>
2022-04-28 17:25:50 +0530

    28Aprl

--- Demo/src/main/java/HelloWorld.java
+++ Demo/src/main/java/HelloWorld.java
@@ -1,12 +1,21 @@
+import java.io.File;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 class HelloWorld {
+	static Logger logger = Logger.getLogger(HelloWorld.class);
 
 	public static void main(String[] args) {
-		
-		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
-		
+
+		String str = "welcome to sonar";
+
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:" + str);
+		logger.warn("warning message...:" + str);
+		logger.error("error message...:" + str);
+
 	}
 
 }
--- test/src/main/java/test/HelloWorld.java
+++ test/src/main/java/test/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String str="HelloWorld";
+System.out.println(str);
 	}
 
 }
