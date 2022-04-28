fc0b56f0d95cc9694b91b96cc3fec3617a47af6e
kirthigavadivelu <104414516+kirthigavadivelu@users.noreply.github.com>
2022-04-28 09:49:14 +0530

    Update HelloWorld.java

--- BehindForce/src/main/java/HelloWorld.java
+++ BehindForce/src/main/java/HelloWorld.java
@@ -1,8 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("HelloWorld");
-		System.out.println("Hi am Kirthiga");
+		System.out.println("HelloWorld");s
 	}
 
 }
