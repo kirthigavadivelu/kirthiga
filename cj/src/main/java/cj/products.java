30b70ab04ba872427d86ef8c520d0369f6221735
kirthiga.vadivelu <kirthiga.vadivelu@DT150152.QICHOAPPL.com>
2022-04-29 12:56:32 +0530

    29April

---
+++ cj/src/main/java/cj/products.java
@@ -0,0 +1,21 @@
+package cj;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class products {
+	static Logger logger = Logger.getLogger(products.class);
+	public static void main(String[] args) {
+		String name = "hi";
+
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:" + name);
+		logger.warn("warning message...:" + name);
+		logger.error("error message...:" + name);
+
+	}
+
+}
