dd537149570259570233f45ff95100a398ce47e4
kirthiga.vadivelu <kirthiga.vadivelu@DT150152.QICHOAPPL.com>
2022-04-29 10:41:36 +0530

    29April

--- test/src/main/java/test/HelloWorld.java
+++ test/src/main/java/test/HelloWorld.java
@@ -1,22 +1,9 @@
 package test;
 
-import java.io.File;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-
-class HelloWorld {
-	static Logger logger = Logger.getLogger(HelloWorld.class);
+public class HelloWorld {
 
 	public static void main(String[] args) {
-
-		String str = "welcome to sonar";
-
-		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
-		PropertyConfigurator.configure(log4jConfigFile);
-		logger.info("this is a message....:" + str);
-		logger.warn("warning message...:" + str);
-		logger.error("error message...:" + str);
+		// TODO Auto-generated method stub
 
 	}
 
